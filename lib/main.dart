8fe219a376c783a12f87dccabed45dbd71a89bc4
RJ2607 <rishijain2607@gmail.com>
2024-10-29 18:14:33 +0530

    Refactor Dock widget to improve drag-and-drop functionality and UI responsiveness

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
 
-/// Entrypoint of the application.
 void main() {
   runApp(const MyApp());
 }
 
-/// [Widget] building the [MaterialApp].
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -20,17 +16,17 @@ class MyApp extends StatelessWidget {
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text(
+              const Text(
                 'Press and hold an item for 2 seconds to drag it to another position.',
               ),
-              SizedBox(height: 16),
-              Dock<Widget>(
+              const SizedBox(height: 16),
+              Dock(
                 items: const [
-                  Center(child: Icon(Icons.person, color: Colors.white)),
-                  Center(child: Icon(Icons.message, color: Colors.white)),
-                  Center(child: Icon(Icons.call, color: Colors.white)),
-                  Center(child: Icon(Icons.camera, color: Colors.white)),
-                  Center(child: Icon(Icons.photo, color: Colors.white)),
+                  Icons.person,
+                  Icons.message,
+                  Icons.call,
+                  Icons.camera,
+                  Icons.photo,
                 ],
                 builder: (e) {
                   return Container(
@@ -42,7 +38,7 @@ class MyApp extends StatelessWidget {
                       color: Colors
                           .primaries[e.hashCode % Colors.primaries.length],
                     ),
-                    child: e,
+                    child: Center(child: Icon(e, color: Colors.white)),
                   );
                 },
               ),
@@ -54,7 +50,6 @@ class MyApp extends StatelessWidget {
   }
 }
 
-/// Dock of the reorderable [items].
 class Dock<T> extends StatefulWidget {
   const Dock({
     super.key,
@@ -62,111 +57,98 @@ class Dock<T> extends StatefulWidget {
     required this.builder,
   });
 
-  /// Initial [T] items to put in this [Dock].
   final List<T> items;
-
-  /// Builder building the provided [T] item.
   final Widget Function(T) builder;
 
   @override
   State<Dock<T>> createState() => _DockState<T>();
 }
 
-/// State of the [Dock] used to manipulate the [_items].
 class _DockState<T> extends State<Dock<T>> {
-  /// [T] items being manipulated.
   late final List<T> _items = widget.items.toList();
-
+  bool _isDragging = false;
+  Widget? _draggedItem;
   int? _draggedIndex;
-  int? _hoveredIndex;
-  final key = UniqueKey();
+  int _hoverIndex = -1; // Initialize to -1 to avoid null checks.
 
   @override
   Widget build(BuildContext context) {
-    return AnimatedContainer(
-      duration: Duration(milliseconds: 300),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: Colors.black12,
-      ),
-      padding: const EdgeInsets.all(4),
-      child: Row(
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: Colors.black12,
+        ),
+        padding: const EdgeInsets.all(4),
+        child: Row(
           mainAxisSize: MainAxisSize.min,
-          children: List.generate(_items.length, (i) {
-            return LongPressDraggable(
-              child: _buildDockItem(i),
-              feedback: Material(
-                child: widget.builder(_items[i]),
-                color: Colors.transparent,
-              ),
-              childWhenDragging:
-                  SizedBox.shrink(), //Hide the dock item when dragging.
+          children: List.generate(_items.length + 1, (i) {
+            if (_hoverIndex == i) {
+              return const SizedBox(
+                width: 60,
+                height: 48,
+              );
+            }
+            final adjustedIndex =
+                i > _hoverIndex && _hoverIndex != -1 ? i - 1 : i;
+            if (adjustedIndex >= _items.length)
+              return const SizedBox.shrink(); // Avoid out-of-range
 
-              data: i,
-              onDragCompleted: () {
-                setState(() {
-                  _draggedIndex = null; // Reset the dragged index.
-                });
-              },
-              onDragStarted: () => setState(() {
-                _draggedIndex = i; // Set the dragged index.
-                _hoveredIndex = null; // Reset the hovered index.
-              }),
-              onDraggableCanceled: (_, __) => setState(() {
-                _draggedIndex = null; // Reset the dragged index.
-                _hoveredIndex = null; // Reset the hovered index.
-              }),
-            );
-          })),
+            return _buildDockItem(adjustedIndex);
+          }),
+        ),
+      ),
     );
   }
 
-/**
- * Builds a dock item at the given index.
- * @param i The index of the item to build.
- * @return The built dock item.
- */
   Widget _buildDockItem(int i) {
     return DragTarget<int>(
-        onAccept: (int index) {
-          setState(() {
-            final item =
-                _items.removeAt(index); // Remove the item from the old index.
-            _items.insert(i, item); // Insert the item at the new index.
-          });
-        },
-        onMove: (details) {
-          setState(() {
-            _hoveredIndex = i; // Set the hovered index.
-          });
-        },
-        onWillAccept: (fromIndex) =>
-            fromIndex != i, // Prevent the item from being dropped on itself.
-        onLeave: (details) {
-          setState(() {
-            _hoveredIndex = null; // Reset the hovered index.
-          });
-        },
-        builder: (context, candidateData, rejectedData) {
-          final con = Container(
-            key: key,
-            padding: const EdgeInsets.all(4),
-            child: Icon(Icons.add, color: Colors.transparent),
-            color: Colors.transparent,
-          );
-          log(_hoveredIndex.toString());
-          if (_draggedIndex == i) {
-            return const SizedBox.shrink(); // Hide the dock item when dragging.
-          }
-          if (_hoveredIndex == i && _hoveredIndex != null) {
-            _items.contains(con as T)
-                ? _items.removeAt(i)
-                : _items.insert(
-                    _hoveredIndex!, con as T); // Reset the hovered index.
-            return widget.builder(_items[i]);
-          } else {
-            return widget.builder(_items[i]);
-          }
+      onAccept: (int index) {
+        setState(() {
+          _isDragging = false;
+          final item = _items.removeAt(index);
+          _items.insert(i, item);
+          _hoverIndex = -1;
         });
+      },
+      onWillAccept: (fromIndex) {
+        setState(() {
+          if (fromIndex != i) _hoverIndex = i;
+        });
+        return fromIndex != i;
+      },
+      onLeave: (index) {
+        setState(() {
+          if (index != null && _isDragging == false) {
+            _isDragging = true;
+
+            _draggedItem = _items.removeAt(index);
+          }
+          _hoverIndex = -1;
+        });
+      },
+      builder: (context, candidateData, rejectedData) {
+        return LongPressDraggable(
+          key: ValueKey(_items[i]),
+          data: i,
+          feedback: Material(
+            child: widget.builder(_items[i]),
+            color: Colors.transparent,
+          ),
+          childWhenDragging: const SizedBox.shrink(),
+          onDragStarted: () => setState(() => _draggedIndex = i),
+          onDraggableCanceled: (_, __) => setState(() {
+            _draggedIndex = null;
+            _hoverIndex = -1;
+          }),
+          onDragCompleted: () => setState(() {
+            _draggedIndex = null;
+            _hoverIndex = -1;
+          }),
+          child: widget.builder(_items[i]),
+        );
+      },
+    );
   }
 }
